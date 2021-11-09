74945372986e47f14c649f01b768b7eb93943307
Thilo Spiertz <thilo.spiertz@valtech.de>
2021-11-09 15:06:19 +0100

    Create entities

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,64 @@
+package guru.springframework.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Notes {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Lob
+	private String recipeNotes;
+	@OneToOne
+	private String recipe;
+	
+	public Notes() {
+		super();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRecipeNotes() {
+		return recipeNotes;
+	}
+	public void setRecipeNotes(String recipeNotes) {
+		this.recipeNotes = recipeNotes;
+	}
+	public String getRecipe() {
+		return recipe;
+	}
+	public void setRecipe(String recipe) {
+		this.recipe = recipe;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Notes other = (Notes) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
