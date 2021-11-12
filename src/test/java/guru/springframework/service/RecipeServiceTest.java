6b0a8615bbee513c8247f0613a1b0e4406480b32
Thilo Spiertz <thilo.spiertz@valtech.de>
2021-11-12 15:31:50 +0100

    Added simple recipe service test

---
+++ src/test/java/guru/springframework/service/RecipeServiceTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repository.RecipeRepository;
+
+public class RecipeServiceTest {
+	
+	private RecipeService recipeService;
+	//private RecipeRepository recipeRepository = Mockito.mock(RecipeRepository.class);
+	@Mock
+	private RecipeRepository recipeRepository;
+	
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		recipeService = new RecipeService(recipeRepository);
+	}
+		
+
+	@Test
+	public void testGetRecipes() {
+		List<Recipe> recipeDummyData = new ArrayList<>();
+		recipeDummyData.add(new Recipe());
+		when(recipeRepository.findAll()).thenReturn(recipeDummyData);
+		
+		List<Recipe> recipes = recipeService.getRecipes();
+		
+		assertEquals(1, recipes.size());
+	}
+
+}
