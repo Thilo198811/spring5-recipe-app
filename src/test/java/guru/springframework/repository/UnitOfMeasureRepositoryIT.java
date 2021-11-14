2b1559b1e057b29d5c867936ce7d5e0d0f77e25b
Thilo Spiertz <thilo.spiertz@valtech.de>
2021-11-14 15:08:36 +0100

    Added database test

---
+++ src/test/java/guru/springframework/repository/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,34 @@
+package guru.springframework.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+	
+	@Autowired
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@BeforeEach
+	public void setup() {
+		
+	}
+	
+	@Test
+	public void testWhenFindByValidDescription_ThenReturnDescription() {
+		assertEquals("Teaspoon", unitOfMeasureRepository.findByDescription("Teaspoon").get().getDescription());
+	}
+	
+	@Test
+	public void testWhenFindByInvalidDescription_ThenOptionalHoldsNoElement() {
+		assertFalse(unitOfMeasureRepository.findByDescription("Teaspoonss").isPresent());
+	}
+}
