823d2d8cd90af638188c14abe442879298f4415b
Thilo Spiertz <thilo.spiertz@valtech.de>
2021-11-14 15:40:25 +0100

    Consistent junit5 annoations

--- src/test/java/guru/springframework/repository/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repository/UnitOfMeasureRepositoryIT.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class UnitOfMeasureRepositoryIT {
 	
