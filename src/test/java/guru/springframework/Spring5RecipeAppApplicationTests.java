3ef23f199415f748d79b2dc6c16301685e6eceaf
Thilo Spiertz <thilo.spiertz@valtech.de>
2021-11-15 14:32:41 +0100

    Fixed sonar issues

--- src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -1,16 +1,24 @@
 package guru.springframework;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Spring5RecipeAppApplicationTests {
+	
+	@Autowired
+	private ApplicationContext ctx;
 
 	@Test
 	public void contextLoads() {
+		assertTrue(ctx != null);
 	}
 
 }
