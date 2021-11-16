c35c23ba0f7be9111bbf04020c72317b75fcd788
Thilo Spiertz <thilo.spiertz@valtech.de>
2021-11-16 13:48:57 +0100

    Added recipe view

--- src/main/java/guru/springframework/controller/RecipeController.java
+++ src/main/java/guru/springframework/controller/RecipeController.java
@@ -3,7 +3,7 @@ package guru.springframework.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import guru.springframework.service.RecipeService;
 
 @Controller
@@ -23,5 +23,10 @@ public class RecipeController {
 		return "recipe";
 	}
 	
+	@GetMapping("/recipes/{id}")
+	public String getRecipeById(@PathVariable Long id, Model model) {
+		model.addAttribute("recipe", recipeService.getRecipeById(id).orElseThrow(IllegalArgumentException::new));
+		return "recipeview";
+	}
 
 }
