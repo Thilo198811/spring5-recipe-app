a78b711a7cf3d998e9f061a4c5d089ef2d9bbe36
Thilo Spiertz <thilo1988@outlook.de>
2021-11-22 23:57:27 +0100

    Increased coverage

--- src/test/java/guru/springframework/service/RecipeServiceTest.java
+++ src/test/java/guru/springframework/service/RecipeServiceTest.java
@@ -83,5 +83,32 @@ class RecipeServiceTest {
 		assertFalse(recipes.get(0).getIngredients().get(1).isAvailabilityInShop()); // Curry
 	}
 	
+	@Test
+	void testWhenShopIsUnavailable_thenReturnFalseAsDefault() {
+		// Given
+		List<Recipe> recipeRepoDummyData = new ArrayList<Recipe>();
+		Recipe recipe = new Recipe();
+		recipe.setDescription("Currywurst");
+		
+		List<Ingredient> ingredients = new ArrayList<Ingredient>();
+		ingredients.add(new Ingredient(BigDecimal.ONE, "Wurst" , new UnitOfMeasure()));
+		ingredients.add(new Ingredient(BigDecimal.ONE, "Curry" , new UnitOfMeasure()));
+		
+		recipe.setIngredients(ingredients);
+		recipeRepoDummyData.add(recipe);
+		
+		when(recipeRepository.findAll()).thenReturn(recipeRepoDummyData);
+		when(proxy.getAllItems()).thenThrow(RuntimeException.class);
+		
+		// When
+		List<Recipe> recipes = recipeService.getRecipes();
+
+		
+		// Then
+		assertFalse(recipes.get(0).getIngredients().get(0).isAvailabilityInShop()); // Wurst
+		assertFalse(recipes.get(0).getIngredients().get(1).isAvailabilityInShop()); // Curry
+
+	}
+	
 
 }
